e565b7daac83ac7e4c1e4b4fb96ef957f58381b3
friedrich15 <emdo2000@gmail.com>
2015-07-24 22:21:17 +0200

    Added some functionalities. Public site now reads DB, yay.

--- hp/public/js/script.js
+++ hp/public/js/script.js
@@ -3,3 +3,16 @@ function enterPage() {
   $("#main-container").fadeIn(800);
   window.history.pushState(null, null, "/home");
 }
+
+function showContent(id, obj) {
+  $(".active").removeClass("active");
+  $(obj).addClass("active");
+  $("#info-text").hide();
+  $.ajax({
+    "type": "POST",
+    "url" : "/show_item",
+    "data": {"id" : id}
+  }).done(function(item){
+    $("#info-text").text(item.contentTextEN).fadeIn();;
+  });
+}
--- hp/routes/index.js
+++ hp/routes/index.js
@@ -1,19 +1,33 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var Item  = mongoose.model('Item');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {
-    title: 'delta web design',
-    enterstate: 'intro'
+  Item.find().sort("position").exec( function ( err, items, count ){
+    res.render('index', {
+      title: 'delta web design',
+      items: items,
+      enterstate: 'intro'
+    });
   });
 });
 
 router.get('/home', function(req, res, next) {
-  res.render('index', {
-    title: 'delta web design',
-    enterstate: 'no-intro'
+  Item.find().sort("position").exec( function ( err, items, count ){
+    res.render('index', {
+      title: 'delta web design',
+      items: items,
+      enterstate: 'no-intro'
+    });
   });
 });
+
+router.post('/show_item', function(req, res, next) {
+  Item.findById(req.body.id, function(err, item) {
+    res.send(item);
+  })
+})
 
 module.exports = router;
